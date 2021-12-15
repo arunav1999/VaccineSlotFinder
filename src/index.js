a6c523c82096044a8cccd374fb46c9bf1022a318
Arunav Dey <arunav.dey@innovaccer.com>
2021-12-15 13:15:25 +0530

    TODO: Restructuring of the codebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import AppComponent from './appcomponent/AppComponent'
+import AppComponent from './components/appcomponent/AppComponent'
 
 
 ReactDOM.render(
